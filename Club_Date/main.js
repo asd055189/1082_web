f44d65b6e0ce973566fdb155ca68dff29d2f6bdd
tpy <tpy@MBA.local>
2020-05-29 09:17:55 +0800

    bug fixed and check utf-8 in the ID tag

--- Club_Date/main.js
+++ Club_Date/main.js
@@ -1,7 +1,4 @@
-$(document).ready(function(){
-    $("#courseTable").append(
-        "<tr><th>場次</th><th>時間</th><th>日期</th></tr>"
-    );                                                      //一次產生固定標題列
+$(document).ready(function(){                                                  
     let topicCount = topicsArray.length;                    //反覆產生資料列
     let oneDayMilliseconds = 24 * 60 * 60 * 1000;           //一天多少毫秒
     /*-----speaker's input version-----
@@ -13,7 +10,7 @@ $(document).ready(function(){
         setMonthAndDay(splitText[1], splitText[2]);
     });*/
 
-    $('#submit').on('click', function(){                    //button submit version
+    $('#查詢營業日期').on('click', function(){                    //button submit version
         let first_date = new Date($('#input_date').val())
         //console.log(first_date);
         let day = first_date.getDate();
@@ -22,17 +19,19 @@ $(document).ready(function(){
         //alert([month, day].join('/'));
         $('#courseTable').empty();//避免之後的submit會使table累加
         setMonthAndDay(month, day);
-        for(let x=0; x<topicCount; x++){//用toLocaleDatString()轉換成year/month/day//用.slice(5)只留month/day
-            let thisDate = new Date(startDate.getTime()+7*x*oneDayMilliseconds);//加豪秒讓時間更改
+        for(let x=0; x<7; x++){//用toLocaleDatString()轉換成year/month/day//用.slice(5)只留month/day
+            let thisDate = new Date(startDate.getTime()+1*x*oneDayMilliseconds);//加豪秒讓時間更改
+            let __day=thisDate.toLocaleDateString().slice(2,-5)-1;
             //將特定row改成灰色
             let trSpecial = "<tr>";
-            if(topicsArray[x]=="國定假日" || topicsArray[x] =="尚未開學")
+            trSpecial = "<tr style='color:green'>";
+            if(topicsArray[__day]=="店休")
                 trSpecial = "<tr style='color:gray'>";
             $('#courseTable').append(
                 trSpecial + 
                 "<td>"+(x+1)+"</td>"+
                 "<td>"+thisDate.toLocaleDateString().slice(0,-5)+"</td>"+
-                "<td>"+topicsArray[x]+"</td>"+
+                "<td >"+topicsArray[__day]+"</td>"+
                 "<tr>"
             );//每一列有場次、預計日期、主題        
         }
--- RandomSelector/main.js
+++ RandomSelector/main.js
@@ -1,21 +1,59 @@
 
 let pictures = [
-    "img/ramen.jpg",
-    "img/rice.jpg",
-    "img/dumplings.jpg"
+    "../茶拿鐵/抹茶拿鐵.jpg",
+    "../茶拿鐵/抹茶咖啡.jpg",
+    "../茶拿鐵/紅茶拿鐵.jpg"
 ];
 
+let pictures2 = [
+    "../星冰樂/咖啡星冰樂.jpg",
+    "../星冰樂/摩卡可可碎片星冰樂.jpg",
+    "../星冰樂/焦糖可可碎片星冰樂.jpg"
+];
+var flag=0;
 $(document).ready(function(){
-    $("input").click(function(){
+
+    $("input#RD").click(function(){
+
         //method1. $("a").hide();//hide all images
         //alert("hi"); 彈跳視窗
         //$("H1").text($("li").eq(1).text()); 將?改成食物名稱, eq為其index
         let num = $("li#food").length;//li有幾個
         //$("H1").text(num);
-        let rdm = Math.floor(Math.random()*num);//0<= Math.random() < 1 //Math.floor() -> 無條件捨去後的最大整數
-        $("H1").text($("li#food").eq(rdm).text());
-        //method1. $("a").eq(rdm).show();       
-        //method2. 
-        $("img").attr("src", pictures[rdm]);
+        if(flag==0){
+            let rdm = Math.floor(Math.random()*num);//0<= Math.random() < 1 //Math.floor() -> 無條件捨去後的最大整數
+            $("H1").text($("li#food").eq(rdm).text());
+            //method1. $("a").eq(rdm).show();       
+            //method2. 
+            $("img").attr("src", pictures[rdm]);
+        }
+        else{
+            let rdm = Math.floor(Math.random()*num);//0<= Math.random() < 1 //Math.floor() -> 無條件捨去後的最大整數
+            $("H1").text($("li#food").eq(rdm).text());
+            //method1. $("a").eq(rdm).show();       
+            //method2. 
+            $("img").attr("src", pictures2[rdm]);
+        }
     });
 });
+
+function change(){
+    flag++;
+    flag%=2;
+    if (flag==1){
+        $("input#change_coffee")[0].value="來點拿鐵"
+        let num = $("li#food").length;
+        $("li#food")[0].textContent="咖啡星冰樂"
+        $("li#food")[1].textContent="摩卡可可碎片星冰樂"
+        $("li#food")[2].textContent="焦糖可可碎片星冰樂"
+        $("img").attr("src", "");
+    }
+    else{
+        $("input#change_coffee")[0].value="來點星冰樂"
+        let num = $("li#food").length;
+        $("li#food")[0].textContent="抹茶拿鐵"
+        $("li#food")[1].textContent="抹茶咖啡"
+        $("li#food")[2].textContent="紅茶拿鐵"
+        $("img").attr("src", "");
+    }
+}
